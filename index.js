0b376dbcf1d547936f0f71d27dd863c6064fa075
Chaitanya82 <chaitanyamohansharma82@gmail.com>
2024-01-12 11:26:20 +0530

    New Commit 120124

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require('express')
 var {MongoClient} = require('mongodb')
 var app = express()
+var cors = require('cors')
+app.use(cors())
+app.use(express.json());
+var url = "mongodb+srv://root:root@atlascluster.fdgbenn.mongodb.net/?retryWrites=true&w=majority"
+var databasename = "books"
 
 var collection = {} // Defining it globally. This is for storing collection
 MongoClient.connect(url).then((client) => { 
     const connect = client.db(databasename); 
     // Connect to collection 
     collection = connect 
-        .collection("introduction"); 
+        .collection("books"); 
   
     collection.find({}).toArray().then((ans) => { 
         console.log(ans); 
@@ -18,9 +23,9 @@ MongoClient.connect(url).then((client) => {
     console.log(err.Message); 
 })
 
-
-app.get('/', (req, res) => {
-    res.send("It's working ..")
-})
-
+app.get('/get',(req, res) => {
+    collection.find({}).toArray().then((ans)=>{
+        res.json(ans)
+    })
+}) 
 app.listen(3000)
